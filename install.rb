d6ea2b0f6123b5376c52943bd1ca7cc44866a92a
Josh Aronson <josh.aronson@skyroo.com>
2010-08-09 18:05:32 -0500

    add install hook

--- install.rb
+++ install.rb
@@ -1,1 +1,8 @@
-# Install hook code here
+require 'rubygems'
+
+this = File.expand_path(File.dirname(__FILE__))
+
+puts "Copying acts_as_attachable/db/migrations to #{RAILS_ROOT}/db/migrations"
+if defined?(RAILS_ROOT)
+  File.copy(File.join(this, "db", "migrations", "0001_create_attachable_assets.rb"), File.join(RAILS_ROOT, "db", "migrations"))
+end
