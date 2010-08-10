374f2d1e05f3f87b56ecb03f52481ab45fa6d90f
Michael Bishop <mik@theablefew.com>
2010-08-10 13:37:17 -0500

    timestamp

--- install.rb
+++ install.rb
@@ -6,6 +6,6 @@ require 'ftools'
 this = File.expand_path(File.dirname(__FILE__))
 puts "Copying acts_as_attachable/db/migrations to #{RAILS_ROOT}/db/migrate"
 if defined?(RAILS_ROOT)
-  time = Time.now.to_i
+  time = Time.now.strftime("%Y%m%d%H%M%S")
   File.copy(File.join(this, "db", "migrate", "0001_create_attachable_assets.rb"), File.join(RAILS_ROOT, "db", "migrate", "#{time}_create_attachable_assets.rb"))
 end
