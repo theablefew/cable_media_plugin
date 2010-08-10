d11e786f162dae9b2867baa0bdec66876ed99879
Michael Bishop <mik@theablefew.com>
2010-08-10 13:24:27 -0500

    Added environment and timestamp to migration install script

--- install.rb
+++ install.rb
@@ -1,8 +1,11 @@
 require 'rubygems'
+require File.dirname(__FILE__) + "/../../../config/environment"
+require 'ftools'
+
 
 this = File.expand_path(File.dirname(__FILE__))
-
-puts "Copying acts_as_attachable/db/migrations to #{RAILS_ROOT}/db/migrations"
+puts "Copying acts_as_attachable/db/migrations to #{RAILS_ROOT}/db/migrate"
 if defined?(RAILS_ROOT)
-  File.copy(File.join(this, "db", "migrations", "0001_create_attachable_assets.rb"), File.join(RAILS_ROOT, "db", "migrations"))
+  time = Time.now.to_i
+  File.copy(File.join(this, "db", "migrate", "0001_create_attachable_assets.rb"), File.join(RAILS_ROOT, "db", "migrate", "#{time}_create_attachable_assets.rb"))
 end
